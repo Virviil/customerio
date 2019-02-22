9e146a355853d50f7f900c28951634313b8546f0
Dmitry Rubinstein <rubinsteindb@gmail.com>
2019-02-23 01:18:41 +0200

    Fix CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Customerio.Mixfile do
     [
       app: :customerio,
       version: "0.2.1",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
